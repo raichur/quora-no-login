0146c89080f70d645a66e3e016f1c206dff555cf
Joshua Raichur <hello@joshuaraichur.com>
2015-07-30 00:08:53 +0530

    Init

---
+++ README.md
@@ -0,0 +1,12 @@
+## Google+ Notification Icon Hider
+
+A simple Chrome extension that hides the Google+ notification icon on Google apps except Google+.
+<img src="show.png">
+
+### Setup:
+
+1. Run `git clone https://github.com/raichur/hide-notifications.git`
+2. `cd` into the directory.
+4. Open Chrome and Click on open up Extensions under the wrench menu.
+5. Check "Developer Mode" and Click "Load Unpacked Extension".
+6. Select the hide-notifications directory.
---
+++ script.js
@@ -0,0 +1,15 @@
+(function(window){
+
+  var arguments = '';
+
+  // If the url has doesn't already have the "share=1" argument
+  if (typeof window.location.search.indexOf('share=1') === -1 || window.location.search === '') {
+    // If there are any other arguments...
+    if (window.location.search.indexOf('?') === 0) {
+      arguments = '?' + window.location.search.substring(1) + '&';
+    }
+    // Append "share=1"
+    arguments += 'share=1';
+    window.location = window.location.origin + window.location.pathname + arguments;
+  }
+})(window);
