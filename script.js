6dd324ddf2b4b9bc53723b770e224f1756517c3b
Joshua Raichur <hello@joshuaraichur.com>
2015-07-30 09:11:26 +0530

    Fix: Script!

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 (function(window){
 
-  var arguments = '';
+  var arguments = '?';
 
   // If the url has doesn't already have the "share=1" argument
   if (typeof window.location.search.indexOf('share=1') === -1 || window.location.search === '') {
     // If there are any other arguments...
     if (window.location.search.indexOf('?') === 0) {
-      arguments = '?' + window.location.search.substring(1) + '&';
+      arguments += window.location.search.substring(1) + '&';
     }
     // Append "share=1"
     arguments += 'share=1';
